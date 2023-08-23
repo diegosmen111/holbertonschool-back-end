92cc8885cc0bfb474af8848eff0f27ef969b4a24
diegosmen111 <dbav_10@hotmail.com>
2023-08-22 17:05:49 -0700

    gather data from an ap

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Python script to export completed tasks data in JSON format
+Usage: python3 0-gather_data_from_an_API.py <employee_id>
+"""
+
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+    sys.exit(1)
+
+employee_id = sys.argv[1]
+url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
+
+response = requests.get(url)
+
+if response.status_code == 404:
+    print('Employee with ID {} does not exist'.format(employee_id))
+    sys.exit(1)
+
+todos = response.json()
+
+completed_tasks = [todo for todo in todos if todo['completed']]
+total_tasks = len(todos)
+
+employee_name = todos[0]['username']
+
+print('Employee {} has completed tasks({}/{}):'.format(employee_name, len(completed_tasks), total_tasks))
+
+for task in completed_tasks:
+    print('\t {} {}'.format(task['title'], '(completed)'))
