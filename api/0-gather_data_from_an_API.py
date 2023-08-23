1ebd48b20639d6813cc9f245fcec62ee1b18f292
Diego Alvarado <87030684+diegosmen111@users.noreply.github.com>
2023-08-22 20:31:14 -0500

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,25 +6,26 @@ import requests
 from sys import argv
 
 
-if __name__ == "__main__":
+def main():
     user_id = argv[1]
 
     # Obtener el nombre de usuario
-    user_url = (
-        f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    )
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
     user_response = requests.get(user_url)
-    username = user_response.json()["name"]
+    user_data = user_response.json()
+    username = user_data["name"]
 
     # Obtener la lista de tareas completadas del usuario
     base_url = "https://jsonplaceholder.typicode.com"
-    todo_url = (
-        f"{base_url}/todos?userId={user_id}&completed=true"
-    )
+    todo_url = f"{base_url}/todos?userId={user_id}&completed=true"
     todo_response = requests.get(todo_url)
     todo_list = todo_response.json()
 
-    # Imprimir la lista de tareas completadas
-    print(f"Employee {username} is done with tasks ({len(todo_list)}):")
+    # Imprimir la información
+    print(f"Employee {username} is done with tasks ({len(todo_list)}/{len(todo_list)}):")
     for todo in todo_list:
         print("\t- " + todo["title"])
+
+
+if __name__ == "__main__":
+    main()
