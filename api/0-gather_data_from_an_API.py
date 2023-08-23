1315fdcc2fcdb86ffac642467821e9a74f2017a4
diegosmen111 <dbav_10@hotmail.com>
2023-08-22 17:13:15 -0700

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,42 @@
 #!/usr/bin/python3
 """
-Python script to export completed tasks data in JSON format
-Usage: python3 0-gather_data_from_an_API.py <employee_id>
+This script retrieves and processes employee tasks data from a REST API.
 """
 
 import requests
 import sys
 
+# Check if the correct number of command-line arguments is provided
 if len(sys.argv) != 2:
     print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
     sys.exit(1)
 
+# Get the employee ID from the command-line argument
 employee_id = sys.argv[1]
 url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
 
+# Make a GET request to the API
 response = requests.get(url)
 
+# Check if the employee exists
 if response.status_code == 404:
-    print('Employee Name: {}'.format(employee_name))
+    print('Employee with ID {} does not exist'.format(employee_id))
     sys.exit(1)
 
+# Extract task data from the response
 todos = response.json()
 
+# Filter completed tasks
 completed_tasks = [todo for todo in todos if todo['completed']]
 total_tasks = len(todos)
 
+# Get the employee's name from the first task
 employee_name = todos[0]['username']
 
-print('Employee {} has completed tasks({}/{}):'.format(employee_name, len(completed_tasks), total_tasks))
+# Print employee and task information
+print('Employee Name: {}'.format(employee_name))
+print('Employee is done with tasks ({}/{}):'.format(len(completed_tasks), total_tasks))
 
+# Print completed tasks
 for task in completed_tasks:
-    for task in completed_tasks:
     print('\t {} {}'.format(task['title'], '(completed)'))
