ababa2b5545540586d9134cc981071ed6172fc02
Diego Alvarado <87030684+diegosmen111@users.noreply.github.com>
2023-08-22 20:18:48 -0500

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,13 +10,17 @@ if __name__ == "__main__":
     user_id = argv[1]
 
     # Obtener el nombre de usuario
-    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    user_url = (
+        f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    )
     user_response = requests.get(user_url)
     username = user_response.json()["name"]
 
     # Obtener la lista de tareas completadas del usuario
     base_url = "https://jsonplaceholder.typicode.com"
-    todo_url = f"{base_url}/todos?userId={user_id}&completed=true"
+    todo_url = (
+        f"{base_url}/todos?userId={user_id}&completed=true"
+    )
     todo_response = requests.get(todo_url)
     todo_list = todo_response.json()
 
@@ -24,6 +28,8 @@ if __name__ == "__main__":
     print(f"Employee Name: {username}")
     print(f"To Do Count: {len(todo_list)}")
     print("First line formatting: OK")
-    print(f"Employee {username} is done with tasks ({len(todo_list)}):")
+    print(
+        f"Employee {username} is done with tasks ({len(todo_list)}):"
+    )
     for todo in todo_list:
         print("\t- " + todo["title"])
